db53f8d047db5df162d1a41bfb14c4d71a7fe085
Kevin Caccamo <kevin@ciinet.org>
2017-11-15 02:12:17 -0500

    Add open and close functionality to project popup

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,62 @@
 document.addEventListener("readystatechange", function (e) {
-  if(document.readyState === "complete") {
+  if (document.readyState === "complete") {
+    /*
     var projItems = document.getElementsByClassName("kc-mywork-item"),
-        projNames = [];
+        projNames = {},
+        projSection = document.getElementById("work-section");
 
     Array.prototype.forEach.call(projItems, function (el) {
       if (el.id) {
-        projNames.push(el.id.substr(7));
+        projNames[el] = el.id.substr(7);
       } else {
         console.warn("No ID for", el);
       }
+    });
+    */
+
+    var projSection = document.getElementById("work-section"),
+        projPopup = document.getElementsByClassName("project-popup")[0],
+        inTransition = false; // Control transitions
+
+    projSection.addEventListener('click', function (e) {
+      if (inTransition) return;
+
+      inTransition = true;
+      function getWorkItem(el) {
+        if (el.classList.contains("kc-mywork-item")) {
+          return el;
+        } else {
+          return getWorkItem(el.parentElement);
+        }
+      }
+
+      var project = getWorkItem(e.target).id.substr(7),
+          projDescs = projPopup.getElementsByClassName("projdesc");
+
+      Array.prototype.forEach.call(projDescs, function (el) {
+        if (el.id.substr(9) === project) {
+          el.classList.remove("noshow");
+        } else {
+          el.classList.add("noshow");
+        }
+      });
+      projPopup.classList.remove("noshow");
+      setTimeout(function () {
+        projPopup.classList.add("up");
+        setTimeout(function () {
+          inTransition = false;
+        }, 1000);
+      }, 10);
+    });
+
+    projPopup.getElementsByClassName("exitbutton")[0].addEventListener("click", function (e) {
+      if (inTransition) return;
+      inTransition = true;
+      projPopup.classList.remove("up");
+      setTimeout(function () {
+        projPopup.classList.add("noshow");
+        inTransition = false;
+      }, 1000);
     });
   }
 });
